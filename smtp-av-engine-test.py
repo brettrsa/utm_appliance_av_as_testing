fc2b89ef60081e435833fb666cea170c90971dd2
brettrsa <brettrsa@gmail.com>
2015-05-02 18:48:04 +0530

    Update smtp-av-engine-test.py

--- smtp-av-engine-test.py
+++ smtp-av-engine-test.py
@@ -9,38 +9,43 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-if len(sys.argv) > 1:
+def engine_test():
 
-	host= sys.argv[1]
-	port = sys.argv[2]
-	mail_from = sys.argv[3]
-	mail_to = sys.argv[4]
+    if len(sys.argv) < 4:
+       print '----------------------------------------------------'
+       print 'Usage: ./smtp.py <host> <port> <mail from> <mail to>'
+       print '----------------------------------------------------'
+    else:
+	   host= sys.argv[1]
+	   port = sys.argv[2]
+	   mail_from = sys.argv[3]
+	   mail_to = sys.argv[4]
 
-	msg = MIMEMultipart()
-	msg['From'] = mail_from
-	msg['To'] = mail_to
-	msg['Subject'] = 'Testing - AV Scanner'
-	message = 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
+	   msg = MIMEMultipart() 
+	   msg['From'] = mail_from
+	   msg['To'] = mail_to
+	   msg['Subject'] = 'Testing - AV Scanner'
+	   message = 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*'
 
-	msg.attach(MIMEText(message))
+	   msg.attach(MIMEText(message)) 
 
-	mailserver = smtplib.SMTP(host,port)
+	   mailserver = smtplib.SMTP(host,port)
 
-	mailserver.set_debuglevel(1)
+	   mailserver.set_debuglevel(1)
 
-	mailserver.ehlo(name='joe')
+	   mailserver.ehlo(name='joe')
 
-	try:
-   	   mailserver.sendmail(mail_from,mail_to,msg.as_string())
-	except smtplib.SMTPDataError:
-	   pass
- 	except smtplib.SMTPRecipientsRefused:
-	   pass
-	finally:
-   	   mailserver.quit()
-else:
-	print '----------------------------------------------------'
-        print 'Usage: ./smtp.py <host> <port> <mail from> <mail to>'
-        print '----------------------------------------------------'
+	   try:
+   	      mailserver.sendmail(mail_from,mail_to,msg.as_string())
+	   except smtplib.SMTPDataError:
+	      pass
+ 	   except smtplib.SMTPRecipientsRefused:
+	      pass
+	   finally:
+   	      mailserver.quit()
+   
+    return
+
+engine_test()
 
 # End
